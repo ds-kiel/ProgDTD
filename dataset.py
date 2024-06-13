810cd87c722200824cab8eda11dd1dc58f83cdeb
Janek Haberer <jha@informatik.uni-kiel.de>
2024-06-13 16:09:09 +0200

    Update license

--- dataset.py
+++ dataset.py
@@ -1,3 +1,8 @@
+# Copyright 2024 Kiel University
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from torchvision import transforms
 from torch.utils.data import Dataset
 import os
